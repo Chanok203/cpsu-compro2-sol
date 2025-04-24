7be5794b79ce931a7571fcf0aaaa00c7cbf87ace
Chanok Pathompatai <cpathompatai@gmail.com>
2025-04-24 20:27:03 +0700

    Calculator_v1

---
+++ src/Calculator_v1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Calculator_v1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] sums = new int[N];
+        int count = 0;
+        int sum = 0;
+        
+        while (true) {
+            int action = sc.nextInt();
+            if (action == 0) {
+                break;
+            }
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            boolean validA = (0 <= a && a < arr.length);
+            boolean validB = (0 <= b && b < arr.length);
+
+            if (validA && validB) {
+                int c = arr[a] + arr[b];
+                System.out.println(c);
+
+                sums[count++] = c;
+                sum += c;
+            } else {
+                System.out.println("error");
+            }
+        }
+        
+        for (int i = 0; i < count; i++) {
+            System.out.print(sums[i] + " ");
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
